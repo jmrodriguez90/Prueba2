a2efaab615b1fbbb52891548a9074521cf0e646f
José Manuel <jmrodriguez90@correo.ugr.es>
2015-11-15 12:18:12 +0100

    borrado de Hello

--- src/cj/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cj;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hola mundo");
-    }
-}
---
+++ src/cj/ProyectoCCACOES.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class ProyectoCCACOES {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+
+    }
+
+}
